9ff8e42831f1a7d23bb314589eda841740cac4f9
MargaritaVin <ritasergienko@rambler.ru>
2022-08-22 13:48:56 +0300

    added class Person and Employee in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,50 @@
 "use strict";
+
+class Person {
+    constructor(firstName, lastName, age, birthDayDate){
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.birthDayDate = new Date(birthDayDate);
+    }
+    showCelebrate() {
+      console.log("Happy Birthday, let’s celebrate");
+    }
+  };
+  
+  class Employee extends Person {
+    #salary = 0;
+    constructor(firstName, lastName, age, birthDayDate, salary, jobPosition) {
+      super(firstName, lastName, age, birthDayDate);
+      this.#salary = salary;
+      this.jobPosition = jobPosition;
+    }
+    getYearSalary() {
+      return this.#salary * 12;
+    }
+    checkCelebrate() {
+      let nowYear = new Date().getFullYear();
+      this.birthDayDate.setFullYear(nowYear);
+      let birthDayInNowYear = this.birthDayDate;
+    
+      function isWeekend(birthDayInNowYear) {
+        const dayOfWeek = new Date(birthDayInNowYear).getDay();
+        return dayOfWeek === 6 || dayOfWeek === 0;
+      };
+  
+      if (isWeekend(birthDayInNowYear) === true) {
+        console.log(super.showCelebrate());
+      } else {
+        console.log("Happy Birthday, but I need to work");
+      }
+    }
+  };
+  
+  const person = new Person("Rita", "Vinokurova", 28, "1994-05-28");
+  console.log (person);
+  person.showCelebrate();
+  
+  const management = new Employee("Anna", "Karenina", 31, "1991-02-07", 15000, "accountant");
+  console.log(management);
+  console.log(`Аccountant's annual salary: ` + management.getYearSalary());
+  management.checkCelebrate();
